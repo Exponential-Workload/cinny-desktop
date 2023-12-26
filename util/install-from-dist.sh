b9f8d0c3d0e57826a650b6c809500701276f83b7
Exponential-Workload <cc@aaathats3as.com>
2023-12-26 05:32:02 +0100

    fix: move it up

--- util/install-from-dist.sh
+++ util/install-from-dist.sh
@@ -15,12 +15,12 @@ mkdir -p $CINNY_INSTALLATION;
 cd $CINNY_INSTALLATION;
 curl -fsSL -o /tmp/cinny.tar.gz https://github.com/Exponential-Workload/cinny-desktop/releases/latest/download/linux-bin-x64.tar.gz
 tar -xzvf /tmp/cinny.tar.gz;
+chmod +x --recusrive $CINNY_INSTALLATION;
 POSTINSTALL=true ./Cinny;
 echo '#!/usr/bin/env bash' > $CINNY_BINARY;
 echo "cd $CINNY_INSTALLATION" >> $CINNY_BINARY;
 echo 'exec ./Cinny' >> $CINNY_BINARY;
 chmod +x $CINNY_BINARY;
-chmod +x --recusrive $CINNY_INSTALLATION;
 if [[ "$USER" != "root" ]]; then
   exec $CINNY_BINARY;
 fi;

149f731ade0fc749dd7e776385630bf4e2c4fc7d
Expo <expo@expo.moe>
2024-06-07 22:16:36 +0200

    fix: for some reason cinny tries to load from /home/[...].wasm

--- src/express.ts
+++ src/express.ts
@@ -5,16 +5,22 @@ export const createApp = () => {
   if (typeof _app === 'undefined') {
     // Create app here | DO NOT CALL .listen()!!
     const app = express();
-    app.use(
-      express.static(path.resolve(__dirname, 'static/app'), {
-        index: 'index.html',
-      }),
-    ); // see webpack main config
-    app.use(
-      express.static(path.resolve(__dirname, 'static'), {
-        index: 'index.html',
-      }),
-    ); // see webpack main config
+    const createAppRoutes = (relativeTo: string) => {
+      app.use(
+        relativeTo,
+        express.static(path.resolve(__dirname, 'static/app'), {
+          index: 'index.html',
+        }),
+      ); // see webpack main config
+      app.use(
+        relativeTo,
+        express.static(path.resolve(__dirname, 'static'), {
+          index: 'index.html',
+        }),
+      ); // see webpack main config
+    };
+    createAppRoutes('/');
+    createAppRoutes('/home/');
 
     /** github kept warning that im doing io without rate limiting so */
     let fsIoRl = 0;

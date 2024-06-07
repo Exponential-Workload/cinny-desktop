02858e5c81c462f133da31e14273bdca121da7ab
Expo <expo@expo.moe>
2024-06-07 21:33:39 +0200

    fix(non-issue): Github complained we aren't rate limiting, so now we are

--- src/express.ts
+++ src/express.ts
@@ -16,11 +16,17 @@ export const createApp = () => {
       }),
     ); // see webpack main config
 
-    app.use((req, res) =>
-      res
-        .status(299)
-        .sendFile(path.resolve(__dirname, 'static', 'app', 'index.html')),
-    );
+    /** github kept warning that im doing io without rate limiting so */
+    let fsIoRl = 0;
+    app.use((req, res) => {
+      fsIoRl++;
+      if (fsIoRl > 100000) return res.status(429).send('what the fuck');
+      else
+        return res
+          .status(299)
+          .sendFile(path.resolve(__dirname, 'static', 'app', 'index.html'));
+    });
+    setInterval(() => (fsIoRl = 0), 1000);
 
     _app = app;
   }
